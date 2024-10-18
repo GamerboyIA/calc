959eedd08a4e184137e3c866b63071a36b6e397e
isaac akins <isaac@ideadrop.com>
2024-10-18 10:35:38 -0400

    10/18 10:35

---
+++ calc.js
@@ -0,0 +1,62 @@
+function one() {
+  document.getElementById("screen").innerHTML += "1";
+}
+
+
+function two() {
+  document.getElementById("screen").innerHTML += "2";
+}
+
+
+function three() {
+  document.getElementById("screen").innerHTML += "3";
+}
+
+
+function four() {
+  document.getElementById("screen").innerHTML += "4";
+}
+
+
+function five() {
+  document.getElementById("screen").innerHTML += "5";
+}
+
+
+function six() {
+  document.getElementById("screen").innerHTML += "6";
+}
+
+
+function seven() {
+  document.getElementById("screen").innerHTML += "7";
+}
+
+
+function eight() {
+  document.getElementById("screen").innerHTML += "8";
+}
+
+
+function nine() {
+  document.getElementById("screen").innerHTML += "9";
+}
+
+
+function zero() {
+  document.getElementById("screen").innerHTML += "0";
+}
+
+
+function plus() {
+  document.getElementById("screen").innerHTML += "+";
+}
+
+
+function minus() {
+  document.getElementById("screen").innerHTML += "-";
+}
+
+
+function equal() {
+  document.getElementById("screen").innerHTML += "="
